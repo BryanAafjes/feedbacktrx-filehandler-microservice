1e302815c44075f616cc7875cd9cb9491b6e342a
BryanAafjes <432731@student.fontys.nl>
2023-04-15 00:44:34 +0200

    Added exception handler

--- feedbacktrx.filehandlermicroservice/Exceptions/ExceptionHandler.cs
+++ feedbacktrx.filehandlermicroservice/Exceptions/ExceptionHandler.cs
@@ -1,6 +1,45 @@
-﻿namespace feedbacktrx.filehandlermicroservice.Exceptions
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace feedbacktrx.filehandlermicroservice.Exceptions
 {
     public class ExceptionHandler
     {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            switch (ex)
+            {
+                //case NotFoundException _:
+                //    response.StatusCode = (int)HttpStatusCode.NotFound;
+                //    break;
+
+                default:
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    await response.WriteAsync(JsonConvert.SerializeObject(new { Error = "An error occurred while processing your request." }));
+                    throw ex;
+            }
+        }
     }
 }
