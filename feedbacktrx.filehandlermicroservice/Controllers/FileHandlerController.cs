e4f6afc1ebbc5cd6bb2223c8102caac0a92028b6
BryanAafjes <432731@student.fontys.nl>
2023-05-24 12:47:40 +0200

    Implemented buffering

--- feedbacktrx.filehandlermicroservice/Controllers/FileHandlerController.cs
+++ feedbacktrx.filehandlermicroservice/Controllers/FileHandlerController.cs
@@ -1,6 +1,7 @@
 ﻿using feedbacktrx.filehandlermicroservice.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 using System.IO;
 
 namespace feedbacktrx.filehandlermicroservice.Controllers
@@ -31,7 +32,20 @@ namespace feedbacktrx.filehandlermicroservice.Controllers
             var mimeType = _service.GetMimeType(fileName);
 
             HttpContext.Response.Headers.Add("Accept-Ranges", "bytes");
+            HttpContext.Response.Headers.Add("Cache-Control", "public, max-age=3600");
 
+            var rangeHeader = HttpContext.Request.Headers["Range"].ToString();
+            var range = RangeHeaderValue.Parse(rangeHeader);
+
+            long start = range.Ranges.FirstOrDefault()?.From ?? 0;
+            long end = range.Ranges.FirstOrDefault()?.To ?? audioStream.Length - 1;
+            long length = end - start + 1;
+
+            HttpContext.Response.StatusCode = 206;
+            HttpContext.Response.Headers.Add("Content-Length", length.ToString());
+            HttpContext.Response.Headers.Add("Content-Range", $"bytes {start}-{end}/{audioStream.Length}");
+
+            audioStream.Seek(start, SeekOrigin.Begin);
             return File(audioStream, mimeType);
         }
     }
