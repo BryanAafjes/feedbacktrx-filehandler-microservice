abcb69004037f74a28274631236a1f3b6d0b207c
BryanAafjes <432731@student.fontys.nl>
2023-05-28 21:47:53 +0200

    Changed clam client

--- feedbacktrx.filehandlermicroservice/Service/ClamAVService.cs
+++ feedbacktrx.filehandlermicroservice/Service/ClamAVService.cs
@@ -1,23 +1,101 @@
-﻿using nClam;
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace feedbacktrx.filehandlermicroservice.Service
 {
-    public class ClamAVService: IClamAVService
+    public enum ClamScanResult
     {
-        private readonly ClamClient _clamAVClient;
+        Clean,
+        Infected,
+        Error
+    }
+
+    public class ClamAVService : IClamAVService
+    {
+        private readonly string _clamAVHost;
+        private readonly int _clamAVPort;
 
         public ClamAVService(string clamAVHost, int clamAVPort)
         {
-            _clamAVClient = new ClamClient(clamAVHost, clamAVPort)
-            {
-                MaxStreamSize = 209715200
-            };
+            _clamAVHost = clamAVHost;
+            _clamAVPort = clamAVPort;
         }
 
         public async Task<ClamScanResult> ScanFileAsync(Stream fileStream)
         {
-            var scanResult = await _clamAVClient.SendAndScanFileAsync(fileStream);
-            return scanResult;
+            using (var tcpClient = new TcpClient(AddressFamily.InterNetwork))
+            {
+                await tcpClient.ConnectAsync(_clamAVHost, _clamAVPort);
+
+                using (var networkStream = tcpClient.GetStream())
+                {
+                    await SendCommandAsync(networkStream, "zINSTREAM");
+
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await networkStream.WriteAsync(buffer, 0, bytesRead);
+                    }
+
+                    await networkStream.WriteAsync(Encoding.ASCII.GetBytes("\0"), 0, 1);
+                    await networkStream.FlushAsync();
+
+                    var response = await ReadResponseAsync(networkStream);
+                    var scanResult = ParseScanResponse(response);
+
+                    return scanResult;
+                }
+            }
+        }
+
+        private async Task SendCommandAsync(NetworkStream networkStream, string command)
+        {
+            byte[] commandBytes = Encoding.ASCII.GetBytes($"{command}\0");
+            await networkStream.WriteAsync(commandBytes, 0, commandBytes.Length);
+            await networkStream.FlushAsync();
+        }
+
+        private async Task<string> ReadResponseAsync(NetworkStream networkStream)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+
+                do
+                {
+                    bytesRead = await networkStream.ReadAsync(buffer, 0, buffer.Length);
+                    await memoryStream.WriteAsync(buffer, 0, bytesRead);
+                }
+                while (networkStream.DataAvailable);
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                using (var streamReader = new StreamReader(memoryStream))
+                {
+                    string response = await streamReader.ReadToEndAsync();
+                    return response;
+                }
+            }
+        }
+
+        private ClamScanResult ParseScanResponse(string response)
+        {
+            if (response.StartsWith("OK"))
+            {
+                return ClamScanResult.Clean;
+            }
+            else if (response.StartsWith("FOUND"))
+            {
+                return ClamScanResult.Infected;
+            }
+            else
+            {
+                return ClamScanResult.Error;
+            }
         }
     }
 }
--- feedbacktrx.filehandlermicroservice/Service/FileHandlerService.cs
+++ feedbacktrx.filehandlermicroservice/Service/FileHandlerService.cs
@@ -31,7 +31,7 @@ namespace feedbacktrx.filehandlermicroservice.Service
             {
                 var scanResult = await _clamAVService.ScanFileAsync(stream);
 
-                if (!scanResult.Result.Equals(ClamScanResults.Clean))
+                if (!scanResult.Equals(ClamScanResults.Clean))
                 {
                     throw new FileNotCleanException("File is not clean!");
                 }
